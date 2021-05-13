8177f5444a2216331db86a89d3c461cf2af46f1f
Marcelo <marcelo.cansilva@gmail.com>
2021-05-13 00:08:57 -0300

    add images favicon and components

--- onebitfood-client/pages/_app.js
+++ onebitfood-client/pages/_app.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
   	<>
   	<Head>
   	<title>OneBitFood V2</title>
+  	<link ref="icon" href="/favicon.icon" />
   	</Head>
   	<main>
   	<Component {...pageProps} />
